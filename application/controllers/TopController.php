d4a6f58a78c39d40697baaf8852a323768224d5e
tadasuke <tadasuke.kato@bitstudio.gr.jp>
2012-11-25 16:23:07 +0900

    20121125_02

--- application/controllers/TopController.php
+++ application/controllers/TopController.php
@@ -6,21 +6,14 @@ class TopController extends BaseController{
 		
 		$userDataClass = new UserData( 'augustus' );
 		$userDataClass = new UserData();
-		$valeuArray = $userDataClass -> getDataByUserId( 1 );
+		$valueArray = $userDataClass -> getDataByUserId( 1 );
 		
-		var_dump( $valeuArray );
+		$userDataArray = array(
+			  'user_id' => $valueArray[0]['user_id']
+			, 'name'    => $valueArray[0]['name']
+		);
 		
-		echo( 'hello akatsuki!!<br/>' );
-		
-		$name = $this -> getGetParam( 'name' );
-		echo( $name . '<br/>' );
-		
-		echo( AK_Registry::get( 'MODE' ) . '<br/>' );
-		
-		$version = $this -> userParam[0];
-		$method  = $this -> userParam[1];
-		echo( $version . '<br/>' );
-		echo( $method  . '<br/>' );
+		$this -> returnJsonResponse( $userDataArray );
 		
 	}
 	
